27b6b763682a8d491e792f6ece03243dab8d595c
Ing. Eduardo E. Enríquez <rekineke@hotmail.com>
2024-06-18 22:06:56 -0500

    creamos funciones de filtrado por cada selector, creamos una funcion que limpia la página y muestra el contenido de acuerdo a la busqueda, y agregamos una función de mostrar un mensaje si lo que se busca no existe, con esto termina el proyecto

--- js/app.js
+++ js/app.js
@@ -23,43 +23,53 @@ const datosBusqueda = {
 
 //Eventos
 document.addEventListener("DOMContentLoaded", () => {
-  mostrarAutos(); //Muestra los autos al cargar la página
+  mostrarAutos(autos); //Muestra los autos al cargar la página
   llenarSelect(); //Llena las opciones de años
 });
 
 //Event listener para los select de búsqueda
 
-marca.addEventListener("change", e => {
+marca.addEventListener("change", (e) => {
   datosBusqueda.marca = e.target.value;
+
+  filtrarAuto();
 });
 
-year.addEventListener("change", e => {
-  datosBusqueda.year = e.target.value;
-})
+year.addEventListener("change", (e) => {
+  datosBusqueda.year = parseInt(e.target.value);
+  filtrarAuto();
+});
 
-minimo.addEventListener("change", e => {
-  datosBusqueda.minimo = e.target.value;
-})
+minimo.addEventListener("change", (e) => {
+  datosBusqueda.minimo = parseInt(e.target.value);
+  filtrarAuto();
+});
 
-maximo.addEventListener("change", e => {
-  datosBusqueda.maximo = e.target.value;
-})
+maximo.addEventListener("change", (e) => {
+  datosBusqueda.maximo = parseInt(e.target.value);
+  filtrarAuto();
+});
 
-puertas.addEventListener("change", e => {
-  datosBusqueda.puertas = e.target.value;
-})
+puertas.addEventListener("change", (e) => {
+  datosBusqueda.puertas = parseInt(e.target.value);
+  filtrarAuto();
+});
 
-transmision.addEventListener("change", e => {
+transmision.addEventListener("change", (e) => {
   datosBusqueda.transmision = e.target.value;
-})
+  filtrarAuto();
+});
 
-color.addEventListener("change", e => {
+color.addEventListener("change", (e) => {
   datosBusqueda.color = e.target.value;
   console.log(datosBusqueda);
-})
+  filtrarAuto();
+});
 
 //Funciones
-function mostrarAutos() {
+function mostrarAutos(autos) {
+  limpiarHtml(); //Elimina el html previo
+
   autos.forEach((auto) => {
     const { marca, modelo, year, precio, puertas, color, transmision } = auto;
     /**El código que estás viendo se llama "destructuring" en JavaScript. Es una forma conveniente de extraer valores de un objeto o elementos de un arreglo y asignarlos a variables individuales. En este caso, se están extrayendo las propiedades marca, modelo, year, precio, puertas, color y transmision del objeto auto y se están asignando a variables con los mismos nombres. Esto permite acceder a cada propiedad de forma más directa y legible en el código. */
@@ -69,6 +79,13 @@ function mostrarAutos() {
     `;
     resultado.appendChild(autoHTML);
   });
+}
+
+// Limpiar HTML
+function limpiarHtml() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
 }
 
 //Genera los años del select
@@ -81,4 +98,89 @@ function llenarSelect() {
     option.textContent = i;
     year.appendChild(option);
   }
+}
+
+// Función que filtra en base a la busqueda
+function filtrarAuto() {
+  const resultado = autos
+    .filter(filtrarmarca)
+    .filter(filtrarYear)
+    .filter(filtrarMinimo)
+    .filter(filtrarMaximo)
+    .filter(filtrarPuertas)
+    .filter(filtrarTransmision)
+    .filter(filtrarColor);
+
+  if (resultado.length) {
+    mostrarAutos(resultado);
+  } else {
+    noResultado();
+  }
+}
+
+/**
+ * Displays a message indicating that no results were found.
+ */
+function noResultado() {
+  limpiarHtml();
+  const noResultado = document.createElement('div');
+  noResultado.classList.add('alerta', 'error');
+  noResultado.textContent = "No se encontraron resultados, intenta con nuevos datos de busqueda.";
+  resultado.appendChild(noResultado);
+}
+
+function filtrarmarca(auto) {
+  const { marca } = datosBusqueda;
+  if (marca) {
+    return auto.marca === marca;
+  }
+  return auto;
+}
+
+function filtrarYear(auto) {
+  const { year } = datosBusqueda;
+  if (year) {
+    return auto.year === year;
+  }
+  return auto;
+}
+
+function filtrarMinimo(auto) {
+  const { minimo } = datosBusqueda;
+  if (minimo) {
+    return auto.precio >= minimo;
+  }
+  return auto;
+}
+
+function filtrarMaximo(auto) {
+  const { maximo } = datosBusqueda;
+  if (maximo) {
+    return auto.precio <= maximo;
+  }
+  return auto;
+}
+
+function filtrarPuertas(auto) {
+  const { puertas } = datosBusqueda;
+  if (puertas) {
+    return auto.puertas === puertas;
+  }
+  return auto;
+}
+
+function filtrarTransmision(auto) {
+  const { transmision } = datosBusqueda;
+  if (transmision) {
+    return auto.transmision === transmision;
+  }
+  return auto;
+}
+
+function filtrarColor(auto) {
+  const { color } = datosBusqueda;
+  if (color) {
+    return auto.color === color;
+  }
+  return auto;
 }
